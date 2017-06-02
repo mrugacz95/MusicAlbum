b39f12f5b6c16be42f44aee7cbae2dba1d60c5a8
Marcin Mrugas <mrugacz95@gmail.com>
2017-06-02 23:36:41 +0200

    Add project files.

---
+++ MusicAlbum/Models/Genre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MusicAlbum.Models
+{
+    public class Genre
+    {
+        [Key]
+        public int GenresId { get; set; }
+        [Display]
+        public string Name { get; set; }
+        public ICollection<Song> Songs { get; set; }
+    }
+}
---
+++ MusicAlbum/Models/MusicDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MusicAlbum.Models
+{
+    public class MusicDbContext : DbContext
+    {
+        public MusicDbContext() : base("DefaultConnection") { }
+        public DbSet<Song> Songs { get; set; }
+
+        public System.Data.Entity.DbSet<MusicAlbum.Models.Genre> Genres { get; set; }
+    }
+}
